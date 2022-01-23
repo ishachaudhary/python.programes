4f421b4f82602ceda861519d9a974fc3d8f96988
ishachaudhary <84828327+ishachaudhary@users.noreply.github.com>
2022-01-23 20:15:45 +0530

    Nth ugly numbers in python

---
+++ 1.py
@@ -0,0 +1,21 @@
+def maxDivide(a, b):
+    while a % b == 0:
+        a = a/b
+    return a
+def isUgly(no):
+    no = maxDivide(no, 2)
+    no = maxDivide(no, 3)
+    no = maxDivide(no, 5)
+    return 1 if no == 1 else 0
+def getNthUglyNo(n):
+    i = 1
+    count = 1
+    while n > count:
+        i = i+1
+        if isUgly(i):
+            count = count+1
+    return i
+no = getNthUglyNo(150)
+print ('The 150th ugly number is', no)
+    
+
